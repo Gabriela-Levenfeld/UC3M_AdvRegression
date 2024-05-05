9505a80ef1831e5ac78ca3aad7ce771d00678174
Gabriela-Levenfeld <43616917+Gabriela-Levenfeld@users.noreply.github.com>
2024-05-06 00:11:37 +0200

    Add all scenarios

--- regularization_procedures.R
+++ regularization_procedures.R
@@ -210,3 +210,300 @@ pred_elasticNet2_y <- predict(finalEnetCV_mod2, newx=x_test2, s=kcvEnet_mod2$lam
 round(error_metrics(pred_Ridge2_y, y_test2), 2)
 round(error_metrics(pred_Lasso2_y, y_test2), 2)
 round(error_metrics(pred_elasticNet2_y, y_test2), 2)
+
+
+
+# Scenario 3 -------------------------------------------------------------------
+simulations3 <- read.csv("syntheticData/scenario3.csv")
+aux3 <- split_data(simulations3)
+
+x_train3 <- as.matrix(subset(aux3$train, select=-c(y)))
+x_test3 <- as.matrix(subset(aux3$test, select=-c(y)))
+
+y_train3 <- aux3$train$y
+y_test3 <- aux3$test$y
+
+## Regularization techniques ----------------------------------------------------
+# One of this models might be used as a benchmark
+ols3 <- lm(y~., data = aux3$train) # OLS
+olsBIC_mod3 <- stepAIC(ols3, k=log(nrow(x_train3)), trace=0) # OLS combined with the BIC
+
+
+# Ridge regression -> alpha=0
+ridge_mod3 <- glmnet(x_train3, y_train3, alpha=0)
+
+# 10-fold cross-validation
+kcvRidge_mod3 <- cv.glmnet(x=x_train3, y=y_train3, alpha=0, nfolds=10)
+kcvRidge_mod3$lambda.min # 0.848094
+length(kcvRidge_mod3$lambda) # 100 which is the last CV position
+# Potential problem! To solve this issue, we extend the range
+
+lambdaGrid1_mod3 <- 10^seq(log10(kcvRidge_mod3$lambda[1]), log10(0.1), length.out=150) # log-spaced grid
+
+kcvRidge2_mod3 <- cv.glmnet(x=x_train3, y=y_train3, alpha=0, nfolds=10, lambda=lambdaGrid1_mod3)
+kcvRidge2_mod3$lambda.min # 0.1
+min(kcvRidge2_mod3$cvm) # The minimum CV error
+# Search lambda 1SE for a trade-off
+kcvRidge2_mod3$lambda.1se # 0.3938968
+
+finalRidgeCV_mod3 <- kcvRidge2_mod3$glmnet.fit
+# Inspect the best models
+plot(finalRidgeCV_mod3, label=TRUE, xvar="lambda")
+abline(v = log(c(kcvRidge2_mod3$lambda.min, kcvRidge2_mod3$lambda.1se)))
+
+# Predictions using the lambda.1se
+pred_Ridge3_coef <- predict(finalRidgeCV_mod3, type="coefficients", s=kcvRidge2_mod3$lambda.1se)
+pred_Ridge3_y <- predict(finalRidgeCV_mod3, newx=x_test3, s=kcvRidge2_mod3$lambda.1se)
+
+# REVIEW: Should we compute the error on the beta coeff?
+
+
+# Lasso regression -> alpha=1
+lasso_mod3 <- glmnet(x=x_train3, y=y_train3, alpha=1)
+kcvLasso_mod3 <- cv.glmnet(x=x_train3, y=y_train3, alpha=1, nfolds=10)
+kcvLasso_mod3$lambda.min
+length(kcvLasso_mod3$lambda)
+# 63, lambda optimal is at the last position of the 10-CV
+# Potential problem! To solve this issue, we extend the range
+
+lambdaGrid2_mod3 <- 10^seq(log10(kcvLasso_mod3$lambda[1]), log10(0.1), length.out=150) # log-spaced grid
+
+kcvLasso2_mod3 <- cv.glmnet(x=x_train3, y=y_train3, alpha=1, nfolds=10, lambda=lambdaGrid2_mod3)
+kcvLasso2_mod3$lambda.min # 0.1
+min(kcvLasso2_mod3$cvm) # The minimum CV error
+# Search lambda 1SE for a trade-off
+kcvLasso2_mod3$lambda.1se # 0.1709883
+
+finalLassoCV_mod3 <- kcvLasso2_mod3$glmnet.fit
+
+# Predictions using the lambda.1se
+pred_Lasso3_coef <- predict(finalLassoCV_mod3, type="coefficients", s=kcvLasso2_mod3$lambda.1se)
+pred_Lasso3_y <- predict(finalLassoCV_mod3, newx=x_test3, s=kcvLasso2_mod3$lambda.1se)
+
+predict(kcvLasso2_mod3, type = "coefficients",
+        s = c(kcvLasso2_mod3$lambda.min, kcvLasso2_mod3$lambda.1se))
+# REVIEW. There is no difference between values... weird!
+
+
+# Elastic Net regression
+elasticNet_mod3 <- caret::train(x_train3, y_train3, method="glmnet",
+                                preProc=c("zv", "center", "scale"),
+                                trControl=trainControl(method="cv", number=10),
+                                tuneLength=10)
+ggplot(elasticNet_mod3, highlight = TRUE)
+
+elasticNet_mod3$bestTune$lambda # 0.04830804
+elasticNet_mod3$bestTune$alpha # 0.1
+
+kcvEnet_mod3 <- cv.glmnet(x=x_train3, y=y_train3, alpha=0.1, nfolds=10)
+# REVIEW: should we fixed the lambda param also?
+finalEnetCV_mod3 <- kcvEnet_mod3$glmnet.fit
+
+# Predictions
+pred_elasticNet3_coef <- predict(finalEnetCV_mod3, s=kcvEnet_mod3$lambda.1se, type="coefficients")
+pred_elasticNet3_y <- predict(finalEnetCV_mod3, newx=x_test3, s=kcvEnet_mod3$lambda.1se)
+
+
+## Errors on test data ---------------------------------------------------------
+# Summary with the errors of three models
+round(error_metrics(pred_Ridge3_y, y_test3), 2)
+round(error_metrics(pred_Lasso3_y, y_test3), 2)
+round(error_metrics(pred_elasticNet3_y, y_test3), 2)
+
+
+
+# Scenario 4 -------------------------------------------------------------------
+simulations4 <- read.csv("syntheticData/scenario4.csv")
+aux4 <- split_data(simulations4)
+
+x_train4 <- as.matrix(subset(aux4$train, select=-c(y)))
+x_test4 <- as.matrix(subset(aux4$test, select=-c(y)))
+
+y_train4 <- aux4$train$y
+y_test4 <- aux4$test$y
+
+## Regularization techniques ----------------------------------------------------
+# One of this models might be used as a benchmark
+ols4 <- lm(y~., data = aux4$train) # OLS
+olsBIC_mod4 <- stepAIC(ols4, k=log(nrow(x_train4)), trace=0) # OLS combined with the BIC
+
+
+# Ridge regression -> alpha=0
+ridge_mod4 <- glmnet(x_train4, y_train4, alpha=0)
+
+# 10-fold cross-validation
+kcvRidge_mod4 <- cv.glmnet(x=x_train4, y=y_train4, alpha=0, nfolds=10)
+kcvRidge_mod4$lambda.min # 0.6629677
+length(kcvRidge_mod4$lambda) # 100 which is the last CV position
+# Potential problem! To solve this issue, we extend the range
+
+lambdaGrid1_mod4 <- 10^seq(log10(kcvRidge_mod4$lambda[1]), log10(0.1), length.out=150) # log-spaced grid
+
+kcvRidge2_mod4 <- cv.glmnet(x=x_train4, y=y_train4, alpha=0, nfolds=10, lambda=lambdaGrid1_mod4)
+kcvRidge2_mod4$lambda.min # 0.1
+min(kcvRidge2_mod4$cvm) # The minimum CV error
+# Search lambda 1SE for a trade-off
+kcvRidge2_mod4$lambda.1se # 0.4119279
+
+finalRidgeCV_mod4 <- kcvRidge2_mod4$glmnet.fit
+# Inspect the best models
+plot(finalRidgeCV_mod4, label=TRUE, xvar="lambda")
+abline(v = log(c(kcvRidge2_mod4$lambda.min, kcvRidge2_mod4$lambda.1se)))
+
+# Predictions using the lambda.1se
+pred_Ridge4_coef <- predict(finalRidgeCV_mod4, type="coefficients", s=kcvRidge2_mod4$lambda.1se)
+pred_Ridge4_y <- predict(finalRidgeCV_mod4, newx=x_test4, s=kcvRidge2_mod4$lambda.1se)
+
+# REVIEW: Should we compute the error on the beta coeff?
+
+
+# Lasso regression -> alpha=1
+lasso_mod4 <- glmnet(x=x_train4, y=y_train4, alpha=1)
+kcvLasso_mod4 <- cv.glmnet(x=x_train4, y=y_train4, alpha=1, nfolds=10)
+kcvLasso_mod4$lambda.min
+length(kcvLasso_mod4$lambda)
+# 66, lambda optimal is at the last position of the 10-CV
+# Potential problem! To solve this issue, we extend the range
+
+lambdaGrid2_mod4 <- 10^seq(log10(kcvLasso_mod4$lambda[1]), log10(0.1), length.out=150) # log-spaced grid
+
+kcvLasso2_mod4 <- cv.glmnet(x=x_train4, y=y_train4, alpha=1, nfolds=10, lambda=lambdaGrid2_mod4)
+kcvLasso2_mod4$lambda.min # 0.1
+min(kcvLasso2_mod4$cvm) # The minimum CV error
+# Search lambda 1SE for a trade-off
+kcvLasso2_mod4$lambda.1se # 0.2604009
+
+finalLassoCV_mod4 <- kcvLasso2_mod4$glmnet.fit
+
+# Predictions using the lambda.1se
+pred_Lasso4_coef <- predict(finalLassoCV_mod4, type="coefficients", s=kcvLasso2_mod4$lambda.1se)
+pred_Lasso4_y <- predict(finalLassoCV_mod4, newx=x_test4, s=kcvLasso2_mod4$lambda.1se)
+
+predict(kcvLasso2_mod4, type = "coefficients",
+        s = c(kcvLasso2_mod4$lambda.min, kcvLasso2_mod4$lambda.1se))
+# There is difference!!
+
+
+# Elastic Net regression
+elasticNet_mod4 <- caret::train(x_train4, y_train4, method="glmnet",
+                                preProc=c("zv", "center", "scale"),
+                                trControl=trainControl(method="cv", number=10),
+                                tuneLength=10)
+ggplot(elasticNet_mod4, highlight = TRUE)
+
+elasticNet_mod4$bestTune$lambda # 0.08723769
+elasticNet_mod4$bestTune$alpha # 0.1
+
+kcvEnet_mod4 <- cv.glmnet(x=x_train4, y=y_train4, alpha=0.1, nfolds=10)
+# REVIEW: should we fixed the lambda param also?
+finalEnetCV_mod4 <- kcvEnet_mod4$glmnet.fit
+
+# Predictions
+pred_elasticNet4_coef <- predict(finalEnetCV_mod4, s=kcvEnet_mod4$lambda.1se, type="coefficients")
+pred_elasticNet4_y <- predict(finalEnetCV_mod4, newx=x_test4, s=kcvEnet_mod4$lambda.1se)
+
+
+## Errors on test data ---------------------------------------------------------
+# Summary with the errors of three models
+round(error_metrics(pred_Ridge4_y, y_test4), 2)
+round(error_metrics(pred_Lasso4_y, y_test4), 2)
+round(error_metrics(pred_elasticNet4_y, y_test4), 2)
+
+
+
+# Scenario 5 -------------------------------------------------------------------
+simulations5 <- read.csv("syntheticData/scenario5.csv")
+aux5 <- split_data(simulations5)
+
+x_train5 <- as.matrix(subset(aux5$train, select=-c(y)))
+x_test5 <- as.matrix(subset(aux5$test, select=-c(y)))
+
+y_train5 <- aux5$train$y
+y_test5 <- aux5$test$y
+
+## Regularization techniques ----------------------------------------------------
+# One of this models might be used as a benchmark
+ols5 <- lm(y~., data = aux5$train) # OLS
+olsBIC_mod5 <- stepAIC(ols5, k=log(nrow(x_train5)), trace=0) # OLS combined with the BIC
+
+
+# Ridge regression -> alpha=0
+ridge_mod5 <- glmnet(x_train5, y_train5, alpha=0)
+
+# 10-fold cross-validation
+kcvRidge_mod5 <- cv.glmnet(x=x_train5, y=y_train5, alpha=0, nfolds=10)
+kcvRidge_mod5$lambda.min # 1.84889
+length(kcvRidge_mod5$lambda) # 100 which is the last CV position
+# Potential problem! To solve this issue, we extend the range
+
+lambdaGrid1_mod5 <- 10^seq(log10(kcvRidge_mod5$lambda[1]), log10(0.1), length.out=150) # log-spaced grid
+
+kcvRidge2_mod5 <- cv.glmnet(x=x_train5, y=y_train5, alpha=0, nfolds=10, lambda=lambdaGrid1_mod5)
+kcvRidge2_mod5$lambda.min # 0.1
+min(kcvRidge2_mod5$cvm) # The minimum CV error
+# Search lambda 1SE for a trade-off
+kcvRidge2_mod5$lambda.1se # 0.2080346
+
+finalRidgeCV_mod5 <- kcvRidge2_mod5$glmnet.fit
+# Inspect the best models
+plot(finalRidgeCV_mod5, label=TRUE, xvar="lambda")
+abline(v = log(c(kcvRidge2_mod5$lambda.min, kcvRidge2_mod5$lambda.1se)))
+
+# Predictions using the lambda.1se
+pred_Ridge5_coef <- predict(finalRidgeCV_mod5, type="coefficients", s=kcvRidge2_mod5$lambda.1se)
+pred_Ridge5_y <- predict(finalRidgeCV_mod5, newx=x_test5, s=kcvRidge2_mod5$lambda.1se)
+
+# REVIEW: Should we compute the error on the beta coeff?
+
+
+# Lasso regression -> alpha=1
+lasso_mod5 <- glmnet(x=x_train5, y=y_train5, alpha=1)
+kcvLasso_mod5 <- cv.glmnet(x=x_train5, y=y_train5, alpha=1, nfolds=10)
+kcvLasso_mod5$lambda.min
+length(kcvLasso_mod5$lambda)
+# 69, lambda optimal is at the last position of the 10-CV
+# Potential problem! To solve this issue, we extend the range
+
+lambdaGrid2_mod5 <- 10^seq(log10(kcvLasso_mod5$lambda[1]), log10(0.1), length.out=150) # log-spaced grid
+
+kcvLasso2_mod5 <- cv.glmnet(x=x_train5, y=y_train5, alpha=1, nfolds=10, lambda=lambdaGrid2_mod5)
+kcvLasso2_mod5$lambda.min # 0.1
+min(kcvLasso2_mod5$cvm) # The minimum CV error
+# Search lambda 1SE for a trade-off
+kcvLasso2_mod5$lambda.1se # 0.1576828
+
+finalLassoCV_mod5 <- kcvLasso2_mod5$glmnet.fit
+
+# Predictions using the lambda.1se
+pred_Lasso5_coef <- predict(finalLassoCV_mod5, type="coefficients", s=kcvLasso2_mod5$lambda.1se)
+pred_Lasso5_y <- predict(finalLassoCV_mod5, newx=x_test5, s=kcvLasso2_mod5$lambda.1se)
+
+predict(kcvLasso2_mod5, type = "coefficients",
+        s = c(kcvLasso2_mod5$lambda.min, kcvLasso2_mod5$lambda.1se))
+# There is difference!!
+
+
+# Elastic Net regression
+elasticNet_mod5 <- caret::train(x_train5, y_train5, method="glmnet",
+                                preProc=c("zv", "center", "scale"),
+                                trControl=trainControl(method="cv", number=10),
+                                tuneLength=10)
+ggplot(elasticNet_mod5, highlight = TRUE)
+
+elasticNet_mod5$bestTune$lambda # 0.01973394
+elasticNet_mod5$bestTune$alpha # 0.2
+
+kcvEnet_mod5 <- cv.glmnet(x=x_train5, y=y_train5, alpha=0.3, nfolds=10)
+# REVIEW: should we fixed the lambda param also?
+finalEnetCV_mod5 <- kcvEnet_mod5$glmnet.fit
+
+# Predictions
+pred_elasticNet5_coef <- predict(finalEnetCV_mod5, s=kcvEnet_mod5$lambda.1se, type="coefficients")
+pred_elasticNet5_y <- predict(finalEnetCV_mod5, newx=x_test5, s=kcvEnet_mod5$lambda.1se)
+
+
+## Errors on test data ---------------------------------------------------------
+# Summary with the errors of three models
+round(error_metrics(pred_Ridge5_y, y_test5), 2)
+round(error_metrics(pred_Lasso5_y, y_test5), 2)
+round(error_metrics(pred_elasticNet5_y, y_test5), 2)
